49736e3ec8906014a3b5423261e40f5a414ded41
Felipe Ferreira de Freitas <fp4303@gmail.com>
2023-02-12 19:05:37 +0000

    atualizado

---
+++ Yao_QC_learning/LS_EDF_QC/learning and share/Loss_Accuracy.jl
@@ -0,0 +1,36 @@
+# for amplitude encoding
+function acc_loss_evaluation(circuit::ChainBlock ,reg::AbstractArrayReg, y_batch::Matrix{Float64},batch_size::Int64, pos_::Int64)
+    res = copy(reg) |> circuit
+    q_ = zeros(batch_size,2);
+    for i=1:batch_size
+        rdm = density_matrix(viewbatch(res, i), (pos_,))
+        q_[i,:] = probs(rdm)
+    end
+    
+    pred = [x[2] for x in argmax(q_,dims=2)[:]]
+    y_max = [x[2] for x in argmax(y_batch,dims=2)[:]]
+    acc = sum(pred .== y_max)/batch_size
+    loss = crossentropy(y_batch,q_)/batch_size
+    acc, loss
+end
+
+# for block encoding
+function acc_loss_evaluation(nbit::Int64, circuit::Vector, y_batch::Matrix{Float64},batch_size::Int64, pos_::Int64)
+    q_ = zeros(batch_size,2);
+    for i=1:batch_size
+        res = zero_state(nbit) |> circuit[i]
+        rdm = density_matrix(res, (pos_,))
+        q_[i,:] = rdm |> probs
+    end
+    
+    pred = [x[2] for x in argmax(q_,dims=2)[:]]
+    y_max = [x[2] for x in argmax(y_batch,dims=2)[:]]
+    acc = sum(pred .== y_max)/batch_size
+    loss = crossentropy(y_batch,q_)/batch_size
+    acc, loss
+end
+
+export crossentropy
+function crossentropy(p, q)
+    return -sum(p .* log.(q))
+end
